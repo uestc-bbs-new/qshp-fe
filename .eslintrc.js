4ee5919ceb77c500e89d737d11b344963dba6c97
rocinate <rocinate1999@gmail.com>
2023-02-10 14:34:53 +0800

    star with ts

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+const react = require('eslint-plugin-react')
+
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'eslint-config-prettier',
+  ],
+  plugins: {
+    react,
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
---
+++ bbs/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <div>teewqr</div>
+    </div>
+  )
+}
+
+export default App
