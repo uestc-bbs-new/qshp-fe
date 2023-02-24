a53a511b447935637cac91c4302106f19653b664
rocinate <rocinate1999@gmail.com>
2023-02-24 21:27:06 +0800

    add editor @ function

--- bbs/src/components/Editor/config.ts
+++ bbs/src/components/Editor/config.ts
@@ -1,7 +1,42 @@
-const options = {
+const options: IOptions = {
   height: 300,
   // change the z-index due to the mui base z-index = 1200
   fullscreen: { index: 1202 },
+  hint: {
+    extend: [
+      {
+        key: '@',
+        hint: (key: string) => {
+          if ('vanessa'.indexOf(key.toLocaleLowerCase()) > -1) {
+            return [
+              {
+                value: '@Vanessa',
+                html: '<img src="https://avatars0.githubusercontent.com/u/970828?s=60&v=4"/> Vanessa',
+              },
+            ]
+          }
+          return []
+        },
+      },
+    ],
+  },
+  upload: {
+    accept: 'image/*,.mp3, .wav, .rar',
+    token: 'test',
+    url: '/api/upload/editor',
+    linkToImgUrl: '/api/upload/fetch',
+    filename(name) {
+      return name
+        .replace(/[^(a-zA-Z0-9\u4e00-\u9fa5.)]/g, '')
+        .replace(/[?\\/:|<>*[]\()\$%\{\}@~]/g, '')
+        .replace('/\\s/g', '')
+    },
+  },
+  counter: {
+    enable: true,
+    type: 'text',
+  },
+
   // toolbar display config
   toolbar: [
     'edit-mode',
@@ -42,9 +77,6 @@ const options = {
     // 'devtools',
     // 'info',
   ],
-  // hint: {
-  //   extend
-  // }
 }
 
 export default options
