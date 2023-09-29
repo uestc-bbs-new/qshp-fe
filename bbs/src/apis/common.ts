94d398d2346f5c1e4cb77d6d61db24373abf0d37
rocinate <rocinate1999@gmail.com>
2023-09-29 21:39:57 +0800

    merge

--- bbs/src/apis/common.ts
+++ bbs/src/apis/common.ts
@@ -1,46 +1,59 @@
-import { BBSInfo, ForumList, Thread, Users, UserInfo, ThreadList } from '@/common/interfaces/response'
+import {
+  BBSInfo,
+  ForumList,
+  Thread,
+  ThreadList,
+  UserInfo,
+  Users,
+} from '@/common/interfaces/response'
 import request from '@/utils/request'
 
 // const commonUrl = 'http://222.197.183.89:65342'
 const commonUrl = ''
 
 export const getForumList = () => {
-  return request.get<null, ForumList>(`${commonUrl}/star/api/forum/v1/view/forum/forum-list`)
+  return request.get<null, ForumList>(
+    `${commonUrl}/star/api/forum/v1/view/forum/forum-list`
+  )
 }
 
 export const getBulletin = (params: object) => {
-  return request.get(`${commonUrl}/star/api/forum/v1/global/thread/bulletin`, { params: params })
+  return request.get(`${commonUrl}/star/api/forum/v1/global/thread/bulletin`, {
+    params: params,
+  })
 }
 
 export const getBBSInfo = () => {
-  return request.get<null, BBSInfo>(`${commonUrl}/star/api/forum/v1/view/forum/bbs-info`)
+  return request.get<null, BBSInfo>(
+    `${commonUrl}/star/api/forum/v1/view/forum/bbs-info`
+  )
 }
 
 export const searchThreads = (params: object) => {
   return request.post<object, { resultNum: number; threads: Thread[] }>(
     `${commonUrl}/star/api/forum/v1/global`,
-    {params: params}
+    { params: params }
   )
 }
 
-export const searchUsers = (params: object) =>{
-  return request.get<object, {total: number; rows: UserInfo[] }>(
+export const searchUsers = (params: object) => {
+  return request.get<object, { total: number; rows: UserInfo[] }>(
     `${commonUrl}/star/api/forum/v1/global/search`,
-     {params: params}
-    )
+    { params: params }
+  )
 }
 
 export const searchUsers_at = (params: object) => {
   console.log(params)
-  return request.get<object, {total: number; rows: Users[] }>(
+  return request.get<object, { total: number; rows: Users[] }>(
     `${commonUrl}/star/api/forum/v1/global/search/at`,
-     {params: params}
-    )
+    { params: params }
+  )
 }
 
 export const getThreadList = (params: object) => {
   return request.get<null, ThreadList>(
     `${commonUrl}/star/api/forum/v1/view/thread/threads`,
-    { params: params}
+    { params: params }
   )
-};
+}
