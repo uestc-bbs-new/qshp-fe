de370ad2e0e52dbe71efd334d0c4fe01ef7c7464
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-11-15 20:10:31 +0800

    Correctly implement link semantics:
    * Navigate to the destination with router without full page reload on click
    * Link href can be copied or opened in a new tab with right clicking.
    * Ctrl+clicking the link opens it in a new and and leave the original page alone.

--- bbs/src/components/Link/index.tsx
+++ bbs/src/components/Link/index.tsx
@@ -1,26 +1,15 @@
-import { useNavigate } from 'react-router-dom'
-
+import { Link as RouterLink } from 'react-router-dom'
 import { Link as MuiLink, LinkProps as MuiLinkProps } from '@mui/material'
-import { MouseEvent, MouseEventHandler, SyntheticEvent } from 'react'
 
 type LinkProps = MuiLinkProps & {
   to?: string
 }
 
 const Link = ({ to, ...other }: LinkProps) => {
-  const navigate = useNavigate()
-
-  const routerNavigate = (e: MouseEvent<HTMLSpanElement, globalThis.MouseEvent>) => {
-    if (to) {
-      navigate(to)
-      e.preventDefault()
-    }
-  }
-
   return (
-    <span onClick={routerNavigate} className="cursor-pointer">
-      <MuiLink href={to} {...other} />
-    </span>
+    <>
+      {to ? <MuiLink component={RouterLink} to={to} {...other} /> : <MuiLink {...other} />}
+    </>
   )
 }
 
