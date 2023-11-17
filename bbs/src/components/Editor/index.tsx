uestc-bbs-new/qshp-fe

commit b58afc2810e35bef24505e2400b4e9cadf40048e

Fix some bugs

bbs/src/components/Editor/index.tsx
@@ -1,7 +1,7 @@
 import Vditor from 'vditor'
 import 'vditor/dist/index.css'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { useAppState } from '@/states'
 
@@ -12,20 +12,26 @@ type props = IOptions & {
 }
 const Editor = ({ setVd, ...other }: props) => {
   const { state } = useAppState()
+  const [vditor, setVditor] = useState<Vditor | undefined>(undefined)
   useEffect(() => {
-    const vditor = new Vditor('vditor', {
+    const vd = new Vditor('vditor', {
       after: () => {
-        setVd(vditor)
+        setVditor(vd)
       },
       ...options,
       ...other,
-      // TODO: the theme only useful when init, can't response to the state.theme change
-      // consider setTheme() instead
       theme: state.theme === 'light' ? 'classic' : 'dark',
-      // TODO: the content theme should be changed when it's dark
-      // contentTheme: state.theme === 'light' ? 'classic' : 'dark',
     })
-  }, [])
+  }, [state.theme])
+
+  useEffect(() => {
+    if (vditor) {
+      vditor.setTheme(
+        state.theme === 'light' ? 'classic' : 'dark',
+        state.theme === 'light' ? 'classic' : 'dark'
+      )
+    }
+  }, [state.theme, vditor])
   return <div id="vditor" className="vditor flex-1" />
 }
 

bbs/src/components/Drawer/index.tsx
@@ -16,7 +16,8 @@ const LeftDrawer = ({ width }: { width: number }) => {
     <>
       <Drawer
         variant={matchesMobile ? 'temporary' : 'persistent'}
-        open={matchesMobile ? !state.drawer : state.drawer}
+        // open={matchesMobile ? !state.drawer : state.drawer}
+        open={state.drawer}
         ModalProps={{
           // Better open performance on mobile.
           keepMounted: matchesMobile,

bbs/src/pages/Home/ForumCover.tsx
@@ -50,7 +50,10 @@ const ForumCover = ({ data }: ForumData) => {
   }
 
   return (
-    <Box className="relative rounded text-white overflow-hidden">
+    <Box
+      className="relative rounded text-white overflow-hidden"
+      style={{ width: '100%' }}
+    >
       <Box
         className="absolute top-0 left-0 h-full w-full"
         style={{
@@ -165,7 +168,7 @@ export const ForumGroup = ({ data }: ForumData) => {
           {data?.forums
             ?.filter((item) => item.name)
             .map((item, index) => (
-              <Grid item md={6} xl={4} key={index}>
+              <Grid item md={6} xl={4} key={index} style={{ width: '100%' }}>
                 <ForumCover data={item} />
               </Grid>
             ))}

commit 387f3905ff588e334381ecd603f297b56c7a2526

Fix bug that prevents posting introduced in b58afc2

bbs/src/components/Editor/index.tsx
@@ -17,6 +17,7 @@ const Editor = ({ setVd, ...other }: props) => {
     const vd = new Vditor('vditor', {
       after: () => {
         setVditor(vd)
+        setVd(vd)
       },
       ...options,
       ...other,