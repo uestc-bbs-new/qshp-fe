uestc-bbs-new/qshp-fe

commit c0dc742310021bd120eda0d443aadaf94ab39fd8

Partial fix of state logic in <Thread>
This fixes the issue that sometimes clicking on threads in the right sidebar does not work for the first time.

bbs/src/pages/Thread/index.tsx
@@ -2,7 +2,7 @@ import Vditor from 'vditor'
 
 import React, { useEffect, useRef, useState } from 'react'
 import { useQuery } from 'react-query'
-import { useLocation, useSearchParams } from 'react-router-dom'
+import { useLocation, useParams, useSearchParams } from 'react-router-dom'
 
 import { Box, Button, Pagination, Stack } from '@mui/material'
 
@@ -21,9 +21,7 @@ function Thread() {
 
   const [searchParams, setSearchParams] = useSearchParams()
   const location = useLocation()
-  const [thread_id, setTread_id] = useState(
-    location.pathname.split('/').pop() as string
-  )
+  const thread_id = useParams()['id'] as string
 
   /**
    * 用于记录页面的 query 参数
@@ -81,21 +79,12 @@ function Thread() {
   }, [info])
 
   useEffect(() => {
-    if ((location.pathname.split('/').pop() as string) !== thread_id) {
-      setTread_id(location.pathname.split('/').pop() as string)
-      setQuery({
-        ...query,
-        page: 1,
-        thread_id: thread_id,
-      })
-    } else {
-      setQuery({
-        ...query,
-        page: Number(searchParams.get('page')) || 1,
-      })
-    }
+    setQuery({
+      ...query,
+      page: Number(searchParams.get('page')) || 1,
+    })
     refetch()
-  }, [location])
+  }, [searchParams, thread_id])
 
   const reply_floor = useRef({
     floor: 1,
@@ -126,10 +115,6 @@ function Thread() {
         page={Number(searchParams.get('page')) || 1}
         onChange={(e, value) => {
           setSearchParams(`page=${value}`)
-          setQuery({
-            ...query,
-            page: value,
-          })
         }}
       />
       {info?.rows ? (

commit 5e00a5432e18a02d22bcfe8b726803e613404013

Fix refresh after post regressed in c0dc742 .

bbs/src/pages/Thread/index.tsx
@@ -22,6 +22,7 @@ function Thread() {
   const [searchParams, setSearchParams] = useSearchParams()
   const location = useLocation()
   const thread_id = useParams()['id'] as string
+  const [replyRefresh, setReplyRefresh] = useState(0)
 
   /**
    * 用于记录页面的 query 参数
@@ -67,6 +68,7 @@ function Thread() {
       )
       vd?.setValue('')
       setSearchParams(`page=${info?.total ? Math.ceil(info?.total / 20) : 10}`)
+      setReplyRefresh(replyRefresh + 1)
     }
   }
 
@@ -84,7 +86,7 @@ function Thread() {
       page: Number(searchParams.get('page')) || 1,
     })
     refetch()
-  }, [searchParams, thread_id])
+  }, [searchParams, thread_id, replyRefresh])
 
   const reply_floor = useRef({
     floor: 1,