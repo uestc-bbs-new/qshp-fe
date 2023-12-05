567a4076b6df867c2f9c0690338b7b51213a6d7c
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-05 22:09:45 +0800

    Extract adoptLegacyAuth function to be reused in a later commit.

--- bbs/src/apis/interceptors/auth_adopt_legacy.ts
+++ bbs/src/apis/interceptors/auth_adopt_legacy.ts
@@ -9,38 +9,43 @@ let delayFurtherRequestsIInterceptorId: number | null = null
 let attempting = false
 let pendingPromise: Promise<void> | null = null
 
+const adoptLegacyAuth = (axios: AxiosInstance) => {
+  if (adoptLegacyAttempted) {
+    return Promise.reject()
+  }
+
+  if (!attempting) {
+    attempting = true
+    pendingPromise = authService
+      .post<object, string>(`${commonUrl}/auth/adoptLegacyAuth`)
+      .then((authorization) => {
+        setAuthorizationHeader(authorization)
+      })
+      .finally(() => {
+        axios.interceptors.request.eject(
+          delayFurtherRequestsIInterceptorId as number
+        )
+        delayFurtherRequestsIInterceptorId = null
+        attempting = false
+        adoptLegacyAttempted = true
+      })
+    delayFurtherRequestsIInterceptorId = axios.interceptors.request.use(
+      async (config) => {
+        await pendingPromise?.catch(() => null)
+        return config
+      }
+    )
+    return pendingPromise.catch(() => Promise.reject())
+  }
+  return Promise.reject()
+}
+
 export default (axios: AxiosInstance) =>
   axios.interceptors.response.use(null, (error: any) => {
     if (error.response?.status === kHttpUnauthorized) {
-      if (adoptLegacyAttempted) {
-        return Promise.reject(error)
-      }
-
-      if (!attempting) {
-        attempting = true
-        pendingPromise = authService
-          .post<object, string>(`${commonUrl}/auth/adoptLegacyAuth`)
-          .then((authorization) => {
-            setAuthorizationHeader(authorization)
-          })
-          .finally(() => {
-            axios.interceptors.request.eject(
-              delayFurtherRequestsIInterceptorId as number
-            )
-            delayFurtherRequestsIInterceptorId = null
-            attempting = false
-            adoptLegacyAttempted = true
-          })
-        delayFurtherRequestsIInterceptorId = axios.interceptors.request.use(
-          async (config) => {
-            await pendingPromise?.catch(() => null)
-            return config
-          }
-        )
-        return pendingPromise
-          .catch(() => Promise.reject(error))
-          .then(() => axios(error.response.config))
-      }
+      return adoptLegacyAuth(axios)
+        .catch(() => Promise.reject(error))
+        .then(() => axios(error.response.config))
     }
     return Promise.reject(error)
   })
