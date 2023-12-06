ad68d8de2ac9d4c2f7adedf1b64982f3d60741b7
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-06 16:10:36 +0800

    Extract useActiveRoute hook.

--- bbs/src/utils/discuzLinkMap.ts
+++ bbs/src/utils/discuzLinkMap.ts
@@ -1,31 +1,15 @@
-import { useEffect, useState } from 'react'
-import { useLocation, useMatches } from 'react-router-dom'
-
+import { useActiveRoute } from './routes'
 import siteRoot from './siteRoot'
 
 const kIndexUrl = `${siteRoot}/forum.php`
 export const useDiscuzLink = () => {
-  const [legacyUrl, setLegacyUrl] = useState(kIndexUrl)
-  const location = useLocation()
-  const matches = useMatches()
-  useEffect(() => {
-    if (matches.length > 0) {
-      const match = matches[matches.length - 1]
-      switch (match.id) {
-        case 'forum':
-          setLegacyUrl(
-            `${siteRoot}/forum.php?mod=forumdisplay&fid=${match.params.id}`
-          )
-          break
-        case 'thread':
-          setLegacyUrl(
-            `${siteRoot}/forum.php?mod=viewthread&tid=${match.params.id}`
-          )
-          break
-        default:
-          setLegacyUrl(kIndexUrl)
-      }
-    }
-  }, [location])
-  return legacyUrl
+  const match = useActiveRoute()
+  switch (match?.id) {
+    case 'forum':
+      return `${siteRoot}/forum.php?mod=forumdisplay&fid=${match.params.id}`
+    case 'thread':
+      return `${siteRoot}/forum.php?mod=viewthread&tid=${match.params.id}`
+    default:
+      return kIndexUrl
+  }
 }
