8d7de0b6c715e21ac8e5d3b3f4ca5bfad2e872c2
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-14 21:17:36 +0800

    Start the attempt to write custom renderers for lute.

--- bbs/src/components/Editor/index.tsx
+++ bbs/src/components/Editor/index.tsx
@@ -10,12 +10,76 @@ import options from './config'
 type props = IOptions & {
   setVd: React.Dispatch<React.SetStateAction<Vditor | undefined>>
 }
+
+type RenderState = {
+  type: 'image' | 'link'
+  text?: string
+  href?: string
+}
+
+const customRenderers = (rendererType: string) => {
+  return (() => {
+    const renderState: RenderState[] = []
+    return {
+      renderImage: (node: ILuteNode, entering: boolean) => {
+        console.log('image', node, entering)
+        if (entering) {
+          renderState.push({ type: 'image' })
+        }
+        return ['', Lute.WalkContinue]
+      },
+      renderLinkText: (node: ILuteNode, entering: boolean) => {
+        console.log('link-text', node, entering, node.TokensStr())
+        if (entering) {
+          if (
+            renderState.length == 0 ||
+            renderState[renderState.length - 1].type != 'image'
+          ) {
+            renderState.push({ type: 'link', text: node.TokensStr() })
+          } else {
+            renderState[renderState.length - 1].text = node.TokensStr()
+          }
+        }
+        return ['', Lute.WalkContinue]
+      },
+      renderLinkDest: (node: ILuteNode, entering: boolean) => {
+        console.log('link-dest', node, entering, node.TokensStr())
+        let html = ''
+        const dest = node.TokensStr()
+        if (entering) {
+          if (renderState.length == 0) {
+            console.error('Unknown render state')
+          } else {
+            const state = renderState[renderState.length - 1]
+            if (state.type == 'image') {
+              renderState.pop()
+              html = `IMG:${state.text}/${dest}`
+            } else if (state.type == 'link') {
+              renderState.pop()
+              html = `<a href="${dest}">${state.text}</a>`
+            } else {
+              console.error('Unknown render state type', state)
+            }
+          }
+        }
+        return [html, Lute.WalkContinue]
+      },
+    }
+  })()
+}
+
 const Editor = ({ setVd, ...other }: props) => {
   const { state } = useAppState()
   const [vditor, setVditor] = useState<Vditor | undefined>(undefined)
   useEffect(() => {
     const vd = new Vditor('vditor', {
       after: () => {
+        // vd.vditor.lute.SetJSRenderers({
+        //   renderers: {
+        //     SpinVditorDOM: customRenderers('SpinVditorDOM'),
+        //     SpinVditorIRDOM: customRenderers('SpinVditorIRDOM'),
+        //   },
+        // })
         setVditor(vd)
         setVd(vd)
       },
