0b1cd5ca4a702cdf6425b2d6df3805e476a98de5
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-18 17:15:29 +0800

    wip attempt to render smily.

--- bbs/src/components/Editor/index.tsx
+++ bbs/src/components/Editor/index.tsx
@@ -4,8 +4,10 @@ import 'vditor/dist/index.css'
 import React, { useEffect, useState } from 'react'
 
 import { useAppState } from '@/states'
+import siteRoot from '@/utils/siteRoot'
 
 import options, { getPreviewThemeOptions } from './config'
+import { smilyMaps } from './smilyData'
 
 type props = IOptions & {
   setVd: React.Dispatch<React.SetStateAction<Vditor | undefined>>
@@ -14,22 +16,184 @@ type props = IOptions & {
 type RenderState = {
   type: 'image' | 'link'
   text?: string
-  href?: string
+  dest?: string
+}
+
+const kForumAttachBasePath = siteRoot + '/data/attachment/forum/'
+const kSmilyBasePath = siteRoot + '/static/image/smiley/'
+function replace(
+  str: string,
+  patterns: RegExp[],
+  replacements: string | string[]
+): string {
+  patterns.forEach((pattern, i) => {
+    const replacement =
+      typeof replacements === 'string' ? replacements : replacements[i]
+    str = str.replace(pattern, replacement)
+  })
+  return str
+}
+// Implemented according to https://www.php.net/manual/en/function.htmlspecialchars.php, without ENT_QUOTES.
+function htmlspecialchars(str: string): string {
+  return replace(
+    str,
+    [/&/g, /"/g, /</g, />/g],
+    ['&amp;', '&quot;', '&lt;', '&gt;']
+  )
+}
+
+function html(strings: TemplateStringsArray, ...texts: string[]): string {
+  return strings
+    .map((chunk, i) =>
+      i < texts.length ? chunk + htmlspecialchars(texts[i]) : chunk
+    )
+    .join('')
+}
+
+const renderImage = (src: string, alt?: string) => {
+  if (src == 's' && smilyMaps['s'][parseInt(alt || '')]) {
+    return `<img src="${kSmilyBasePath}${
+      smilyMaps['s'][parseInt(alt || '')]
+    }" class="smily" data-x-special-kind="smily" data-x-original-src="${src}" data-x-original-alt="${alt}">`
+  }
+  const match = src.match(/^a:([0-9]+)/)
+  if (match) {
+    // attachment
+    return ''
+  }
+  return html`<img src="${src}" alt="${alt || ''}" />`
+}
+const renderLink = (href: string, text: string) => {
+  return ''
+}
+
+const shouldRenderMarkers = (
+  nodeType: string,
+  rendererType: string,
+  node: ILuteNode,
+  entering: boolean
+) => {
+  // See also lute/render/vditor_{ir,sv}_renderer.go for reference implementation.
+  // `node.Parent.LinkType == 3` is not implemented yet.
+  if (
+    (rendererType == 'SpinVditorIRDOM' || rendererType == 'SpinVditorSVDOM') &&
+    entering
+  ) {
+    const mode = rendererType == 'SpinVditorIRDOM' ? 'ir' : 'sv'
+    const lex = ['linkText', 'linkDest', 'image'].includes(nodeType)
+      ? node.TokensStr()
+      : nodeType
+    const extraClass =
+      {
+        '[': ` vditor-${mode}__marker--bracket`,
+        ']': ` vditor-${mode}__marker--bracket`,
+        linkText: ` vditor-${mode}__marker--bracket`,
+        '(': ` vditor-${mode}__marker--paren`,
+        ')': ` vditor-${mode}__marker--paren`,
+        linkDest: ` vditor-${mode}__marker--link`,
+      }[nodeType] || ''
+    if (nodeType == 'image' && rendererType == 'SpinVditorIRDOM') {
+      return [
+        entering
+          ? `<span class="vditor-ir__node vditor-ir__node--expand" data-type="img">`
+          : `</span>`,
+        Lute.WalkContinue,
+      ]
+    }
+    return [
+      `<span class="vditor-${mode}__marker${extraClass}">${lex}</span>`,
+      Lute.WalkContinue,
+    ]
+  }
+  return false
 }
 
 const customRenderers = (rendererType: string) => {
-  return (() => {
+  return ((rendererType: string) => {
     const renderState: RenderState[] = []
     return {
+      renderBang: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, 'bang', node, entering)
+        return (
+          shouldRenderMarkers('!', rendererType, node, entering) || [
+            '',
+            Lute.WalkContinue,
+          ]
+        )
+      },
+      renderLink: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, 'link', node, entering)
+        return ['', Lute.WalkContinue]
+      },
+      renderOpenBracket: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, '[', node, entering)
+        return (
+          shouldRenderMarkers('[', rendererType, node, entering) || [
+            '',
+            Lute.WalkContinue,
+          ]
+        )
+      },
+      renderCloseBracket: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, ']', node, entering)
+        return (
+          shouldRenderMarkers(']', rendererType, node, entering) || [
+            '',
+            Lute.WalkContinue,
+          ]
+        )
+      },
+      renderOpenParen: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, '(', node, entering)
+        return (
+          shouldRenderMarkers('(', rendererType, node, entering) || [
+            '',
+            Lute.WalkContinue,
+          ]
+        )
+      },
+      renderCloseParen: (node: ILuteNode, entering: boolean) => {
+        console.log(rendererType, ')', node, entering)
+        if (entering) {
+          return (
+            shouldRenderMarkers(')', rendererType, node, entering) || [
+              '',
+              Lute.WalkContinue,
+            ]
+          )
+        } else {
+          let html = ''
+          if (renderState.length == 0) {
+            console.error('Unknown render state')
+          } else {
+            const state = renderState[renderState.length - 1]
+            if (state.type == 'image') {
+              renderState.pop()
+              html = renderImage(state.dest, state.text)
+            } else if (state.type == 'link') {
+              renderState.pop()
+              html = renderLink(state.dest, state.text)
+            } else {
+              console.error('Unknown render state type', state)
+            }
+          }
+          return [html, Lute.WalkContinue]
+        }
+      },
       renderImage: (node: ILuteNode, entering: boolean) => {
-        console.log('image', node, entering)
+        console.log(rendererType, 'image', node, entering)
         if (entering) {
           renderState.push({ type: 'image' })
         }
-        return ['', Lute.WalkContinue]
+        return (
+          shouldRenderMarkers('image', rendererType, node, entering) || [
+            '',
+            Lute.WalkContinue,
+          ]
+        )
       },
       renderLinkText: (node: ILuteNode, entering: boolean) => {
-        console.log('link-text', node, entering, node.TokensStr())
+        console.log(rendererType, 'link-text', node, entering, node.TokensStr())
         if (entering) {
           if (
             renderState.length == 0 ||
@@ -39,33 +203,40 @@ const customRenderers = (rendererType: string) => {
           } else {
             renderState[renderState.length - 1].text = node.TokensStr()
           }
+          return (
+            shouldRenderMarkers('linkDest', rendererType, node, entering) || [
+              html,
+              Lute.WalkContinue,
+            ]
+          )
         }
         return ['', Lute.WalkContinue]
       },
       renderLinkDest: (node: ILuteNode, entering: boolean) => {
-        console.log('link-dest', node, entering, node.TokensStr())
-        let html = ''
-        const dest = node.TokensStr()
+        console.log(rendererType, 'link-dest', node, entering, node.TokensStr())
         if (entering) {
+          return (
+            shouldRenderMarkers('linkDest', rendererType, node, entering) || [
+              '',
+              Lute.WalkContinue,
+            ]
+          )
+        } else {
           if (renderState.length == 0) {
             console.error('Unknown render state')
           } else {
             const state = renderState[renderState.length - 1]
-            if (state.type == 'image') {
-              renderState.pop()
-              html = `IMG:${state.text}/${dest}`
-            } else if (state.type == 'link') {
-              renderState.pop()
-              html = `<a href="${dest}">${state.text}</a>`
+            if (state.type == 'image' || state.type == 'link') {
+              state.dest = node.TokensStr()
             } else {
               console.error('Unknown render state type', state)
             }
           }
+          return ['', Lute.WalkContinue]
         }
-        return [html, Lute.WalkContinue]
       },
     }
-  })()
+  })(rendererType)
 }
 
 const Editor = ({ setVd, ...other }: props) => {
@@ -75,14 +246,16 @@ const Editor = ({ setVd, ...other }: props) => {
   useEffect(() => {
     const vd = new Vditor('vditor', {
       after: () => {
-        // vd.vditor.lute.SetJSRenderers({
-        //   renderers: {
-        //     SpinVditorDOM: customRenderers('SpinVditorDOM'),
-        //     SpinVditorIRDOM: customRenderers('SpinVditorIRDOM'),
-        //   },
-        // })
+        vd.vditor.lute.SetJSRenderers({
+          renderers: {
+            SpinVditorDOM: customRenderers('SpinVditorDOM'),
+            SpinVditorIRDOM: customRenderers('SpinVditorIRDOM'),
+            SpinVditorSVDOM: customRenderers('SpinVditorSVDOM'),
+          } as object,
+        })
         setVditor(vd)
         setVd(vd)
+        window.vd = vd
       },
       ...options,
       preview: getPreviewThemeOptions(state.theme),
