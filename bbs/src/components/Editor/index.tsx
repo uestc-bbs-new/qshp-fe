925e3c7797b52e3f49b0914c9d4ef58d969c5ddf
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-19 17:07:34 +0800

    Set dialog size.

--- bbs/src/components/Editor/index.tsx
+++ bbs/src/components/Editor/index.tsx
@@ -3,7 +3,15 @@ import 'vditor/dist/index.css'
 
 import React, { useEffect, useRef, useState } from 'react'
 
-import { Divider, Grid, IconButton, Menu, Tab, Tabs } from '@mui/material'
+import {
+  Divider,
+  Grid,
+  IconButton,
+  Menu,
+  Stack,
+  Tab,
+  Tabs,
+} from '@mui/material'
 
 import { useAppState } from '@/states'
 
@@ -19,7 +27,7 @@ const Editor = ({ setVd, ...other }: props) => {
   const { state } = useAppState()
   const [vditor, setVditor] = useState<Vditor | undefined>(undefined)
   const theme = state.theme === 'light' ? undefined : 'dark'
-  const smilyAnchor = useRef()
+  const smilyAnchor = useRef<HTMLElement>()
   const [smilyOpen, setSmilyOpen] = useState(false)
   const closeSmily = () => setSmilyOpen(false)
   const [selectedSmilyKind, setSmilyKind] = useState(smilyData[0])
@@ -49,7 +57,7 @@ const Editor = ({ setVd, ...other }: props) => {
           tip: '表情',
           icon: '<svg viewBox="0 0 24 24"><path d="M11.99 2C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"></path></svg>',
           click: (e) => {
-            smilyAnchor.current = e.target
+            smilyAnchor.current = e.target as HTMLElement
             setSmilyOpen(true)
           },
         },
@@ -73,31 +81,34 @@ const Editor = ({ setVd, ...other }: props) => {
         open={smilyOpen}
         onClose={closeSmily}
       >
-        <Tabs
-          value={selectedSmilyKind}
-          onChange={(_, newValue) => setSmilyKind(newValue)}
-        >
-          {smilyData.map((smilyKind, index) => (
-            <Tab key={index} label={smilyKind.name} value={smilyKind}></Tab>
-          ))}
-        </Tabs>
-        <Divider />
-        <Grid container>
-          {selectedSmilyKind.items.map((item, index) => (
-            <Grid key={index} item>
-              <IconButton
-                onClick={() => {
-                  vditor?.insertValue(` ![${item.code || item.id}](s) `)
-                  closeSmily()
-                }}
-              >
-                <img
-                  src={`${kSmilyBasePath}/${selectedSmilyKind.path}/${item.path}`}
-                />
-              </IconButton>
-            </Grid>
-          ))}
-        </Grid>
+        <Stack maxWidth={640}>
+          <Tabs
+            value={selectedSmilyKind}
+            onChange={(_, newValue) => setSmilyKind(newValue)}
+          >
+            {smilyData.map((smilyKind, index) => (
+              <Tab key={index} label={smilyKind.name} value={smilyKind}></Tab>
+            ))}
+          </Tabs>
+          <Divider />
+          <Grid container maxHeight="300px" overflow="auto" flexShrink={1}>
+            {selectedSmilyKind.items.map((item, index) => (
+              <Grid key={index} item>
+                <IconButton
+                  onClick={() => {
+                    vditor?.insertValue(` ![${item.code || item.id}](s) `)
+                    closeSmily()
+                  }}
+                >
+                  <img
+                    src={`${kSmilyBasePath}/${selectedSmilyKind.path}/${item.path}`}
+                    style={{ width: `${item.width}px` }}
+                  />
+                </IconButton>
+              </Grid>
+            ))}
+          </Grid>
+        </Stack>
       </Menu>
     </>
   )
