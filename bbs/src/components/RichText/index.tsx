8b9f9a14f96dfd4dcb5379ce8d10c3dec6e968c9
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-01-25 17:19:46 +0800

    poc for richtext color adaptation for dark mode.

--- bbs/src/components/RichText/index.tsx
+++ bbs/src/components/RichText/index.tsx
@@ -2,7 +2,7 @@ import Vditor from 'vditor'
 
 import { createRef, useEffect } from 'react'
 
-import { Typography } from '@mui/material'
+import { Typography, darken, getContrastRatio, lighten } from '@mui/material'
 
 import { PostFloor } from '@/common/interfaces/response'
 import { getPreviewOptions } from '@/components/RichText/vditorConfig'
@@ -13,9 +13,38 @@ import bbcode2html from '@/utils/bbcode/bbcode'
 
 import './richtext.css'
 
+const kAuthoredColor = 'authoredColor'
+
 const LegacyPostRenderer = ({ post }: { post: PostFloor }) => {
+  const { state } = useAppState()
+  const contentRef = createRef<HTMLDivElement>()
+  useEffect(() => {
+    if (contentRef.current) {
+      ;[].forEach.call(
+        contentRef.current.querySelectorAll('font, *[style]'),
+        (el: HTMLElement) => {
+          const color = el.dataset[kAuthoredColor] || getComputedStyle(el).color
+          if (!color) {
+            return
+          }
+          const contrast = getContrastRatio(
+            color,
+            state.theme == 'light' ? '#ffffff' : '#313742'
+          )
+          const contrastThreshold = state.theme == 'light' ? 1 : 4
+          if (contrast < contrastThreshold) {
+            const newColor =
+              state.theme == 'light' ? darken(color, 0.2) : lighten(color, 0.5)
+            el.style.color = newColor
+            el.dataset[kAuthoredColor] = newColor
+          }
+        }
+      )
+    }
+  }, [state.theme])
   return (
     <div
+      ref={contentRef}
       className="rich-text-content rich-text-content-legacy"
       dangerouslySetInnerHTML={{
         __html: bbcode2html(post.message, {
