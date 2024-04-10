f7e1f79f6aa97337e236313da61cc8f29633ddb2
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-04-10 14:06:20 +0800

    Style image view dialog.

--- bbs/src/dialogs/ImageViewDialog/index.tsx
+++ bbs/src/dialogs/ImageViewDialog/index.tsx
@@ -1,3 +1,6 @@
+import { css } from '@emotion/react'
+
+import { useState } from 'react'
 import { TransformComponent, TransformWrapper } from 'react-zoom-pan-pinch'
 
 import { Close } from '@mui/icons-material'
@@ -15,22 +18,71 @@ const ImageViewDialog = ({
   open: boolean
   onClose?: () => void
   singleImage?: ImageItem
-}) => (
-  <Dialog open={open} onClose={onClose} fullScreen>
-    <TransformWrapper>
-      <TransformComponent>
-        <Stack justifyContent="center" alignItems="center">
-          {singleImage && <img src={singleImage.fullUrl} />}
-        </Stack>
-      </TransformComponent>
-    </TransformWrapper>
-    <IconButton
-      sx={{ position: 'absolute', right: 10, top: 10 }}
-      onClick={onClose}
+}) => {
+  const fullSizeCss = { width: '100%', height: '100%' }
+  const [panning, setPanning] = useState(false)
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      fullScreen
+      PaperProps={{
+        sx: { backgroundColor: 'rgba(0, 0, 0, 0.6)' },
+      }}
     >
-      <Close />
-    </IconButton>
-  </Dialog>
-)
+      <TransformWrapper
+        centerOnInit
+        centerZoomedOut
+        minScale={0.5}
+        onPanningStart={() => setPanning(true)}
+        onPanningStop={() => setPanning(false)}
+      >
+        <TransformComponent
+          wrapperStyle={{ ...fullSizeCss }}
+          contentStyle={{
+            ...fullSizeCss,
+            cursor: panning ? 'grabbing' : 'grab',
+          }}
+        >
+          <Stack
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              width: '100%',
+              height: '100%',
+            }}
+          >
+            {singleImage && (
+              <img
+                src={singleImage.fullUrl}
+                css={css({
+                  width: 'auto',
+                  height: 'auto',
+                  maxWidth: '100%',
+                  maxHeight: '100%',
+                })}
+              />
+            )}
+          </Stack>
+        </TransformComponent>
+      </TransformWrapper>
+      <IconButton
+        sx={{
+          position: 'absolute',
+          right: 10,
+          top: 10,
+          boxShadow: '0 0 32px rgba(0, 0, 0, 0.5)',
+          backgroundColor: 'white',
+          '&:hover': {
+            backgroundColor: 'white',
+          },
+        }}
+        onClick={onClose}
+      >
+        <Close />
+      </IconButton>
+    </Dialog>
+  )
+}
 
 export default ImageViewDialog
