11dfda3f6cb819c10dffec2955912c0d937bd84a
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-04-15 20:46:29 +0800

    Test vditor preview.

--- legacy/src/main.ts
+++ legacy/src/main.ts
@@ -1,6 +1,19 @@
+import Vditor from 'vditor/dist/method.min.js'
+
 ;[].forEach.call(
   document.querySelectorAll('#postlist table.plhin'),
   (post: Element) => {
-    console.log(post.querySelector('.pct .pcb td.t_f')?.textContent)
+    const content = post.querySelector('.pct .pcb td.t_f')
+    if (!content) {
+      return
+    }
+    const markdown = content.textContent || ''
+    console.log(markdown, content)
+    if (markdown) {
+      content.innerHTML = '<div></div>'
+      Vditor.preview(content.querySelector('div') as HTMLDivElement, markdown, {
+        mode: 'light',
+      })
+    }
   }
 )
