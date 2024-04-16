5baa9b93fbf411a6cfebf6b3fa8c1a65e8fefca5
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-04-16 09:28:49 +0800

    Update selectors now that uestc-bbs-new/old-bbs@0ba7650 is committed.

--- legacy/src/main.ts
+++ legacy/src/main.ts
@@ -3,15 +3,15 @@ import Vditor from 'vditor/dist/method.min.js'
 ;[].forEach.call(
   document.querySelectorAll('#postlist table.plhin'),
   (post: Element) => {
-    const content = post.querySelector('.pct .pcb td.t_f')
+    const content = post.querySelector('.post-message-markdown')
     if (!content) {
       return
     }
     const markdown = content.textContent || ''
     console.log(markdown, content)
     if (markdown) {
-      content.innerHTML = '<div></div>'
-      Vditor.preview(content.querySelector('div') as HTMLDivElement, markdown, {
+      content.innerHTML = ''
+      Vditor.preview(content, markdown, {
         mode: 'light',
       })
     }
