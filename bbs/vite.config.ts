2445997a57ac39d537823159c307a89b79222681
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-05-06 07:42:36 +0800

    Include commonjsHelpers.js in vendor-react chunk to make sure it does not import from any other chunks.

--- bbs/vite.config.ts
+++ bbs/vite.config.ts
@@ -27,7 +27,9 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (
-            // id.includes('node_modules/react-dom')
+            id.includes('commonjsHelpers.js') ||
+            id.includes('node_modules/react/') ||
+            id.includes('node_modules/react-dom') ||
             id.includes('node_modules/react-router') ||
             id.includes('node_modules/@remix-run')
           ) {
