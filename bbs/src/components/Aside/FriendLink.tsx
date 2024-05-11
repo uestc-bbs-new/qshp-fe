8f8339589524daff8ee7e4daa36ecf9f5ca2ab6f
Akejyo <987383393@qq.com>
2024-05-11 14:53:55 +0800

    remove qshp link

--- bbs/src/components/Aside/FriendLink.tsx
+++ bbs/src/components/Aside/FriendLink.tsx
@@ -32,21 +32,12 @@ const FriendLink = () => {
           友情链接
         </Typography>
       </Box>
-      <Link
-        to={'https://bbs.uestc.edu.cn/'}
-        external={true}
-        target={'_blank'}
-        underline="none"
-      >
-        <Box display="flex" alignItems={'center'} className="mb-3">
-          <img src={qshpLogoImg} alt="清水河畔" className="w-5 h-5 mr-1" />
-          <Typography
-            sx={{ fontSize: 13, color: theme.palette.text.secondary }}
-          >
-            清水河畔-电子科技大学官方论坛
-          </Typography>
-        </Box>
-      </Link>
+      <Box display="flex" alignItems={'center'} className="mb-3">
+        <img src={qshpLogoImg} alt="清水河畔" className="w-5 h-5 mr-1" />
+        <Typography sx={{ fontSize: 13, color: theme.palette.text.secondary }}>
+          清水河畔-电子科技大学官方论坛
+        </Typography>
+      </Box>
       <Link
         to={'https://www.uestc.edu.cn/'}
         external={true}
