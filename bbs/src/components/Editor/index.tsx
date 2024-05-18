62ddd07683928252a7195826dc8602cf6e83716f
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-05-18 14:51:52 +0800

    Override vditor's large spacing for toolbar items on mobile.

--- bbs/src/components/Editor/index.tsx
+++ bbs/src/components/Editor/index.tsx
@@ -126,7 +126,18 @@ const Editor = forwardRef<EditorHandle, EditorProps>(function Editor(
   )
   return (
     <>
-      <div ref={vditorRef} className="vditor flex-1" onKeyDown={onKeyDown} />
+      <div
+        ref={vditorRef}
+        className="vditor flex-1"
+        css={{
+          '@media (max-width: 520px)': {
+            '.vditor-toolbar__item': {
+              padding: 0,
+            },
+          },
+        }}
+        onKeyDown={onKeyDown}
+      />
       <Menu
         anchorEl={smilyAnchor.current}
         open={smilyOpen}
