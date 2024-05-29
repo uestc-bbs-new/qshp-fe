uestc-bbs-new/qshp-fe

commit dede9ddc16013b31ab9476a22a5ae166b34dee2f

Remove slide transition in TopListView to improve performance.

bbs/src/components/TopList/TopListView.tsx
@@ -22,7 +22,6 @@ import {
   List,
   Paper,
   Skeleton,
-  Slide,
   Stack,
   Tab,
   Tabs,
@@ -354,7 +353,7 @@ export const TopListDialog = ({
     return () => void (document.body.style.overflow = '')
   }, [open])
 
-  const body = (
+  return (
     <Paper
       sx={{
         left: 0,
@@ -364,26 +363,15 @@ export const TopListDialog = ({
         position: 'fixed',
         zIndex: 1,
       }}
-      hidden={(alwaysOpen || noTransition) && !open}
+      hidden={!open}
     >
       <TopListView
-        onClose={
-          alwaysOpen
-            ? undefined
-            : () => {
-                document.body.style.overflow = ''
-                onClose()
-              }
-        }
+        onClose={() => {
+          document.body.style.overflow = ''
+          onClose()
+        }}
       />
     </Paper>
-  )
-  return alwaysOpen || noTransition ? (
-    body
-  ) : (
-    <Slide in={open} direction="up">
-      {body}
-    </Slide>
   )
 }
 

commit 2cff13f6014858891e1b821ee9b76cdd61a76277

Fix close button regressed in dede9dd.

bbs/src/components/TopList/TopListView.tsx
@@ -366,10 +366,14 @@ export const TopListDialog = ({
       hidden={!open}
     >
       <TopListView
-        onClose={() => {
-          document.body.style.overflow = ''
-          onClose()
-        }}
+        onClose={
+          alwaysOpen
+            ? undefined
+            : () => {
+                document.body.style.overflow = ''
+                onClose()
+              }
+        }
       />
     </Paper>
   )