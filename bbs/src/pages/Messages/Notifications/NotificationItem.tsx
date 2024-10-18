d605044e42326ffcdc5c08a0c8e442c9f8b22126
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-10-18 22:23:32 +0800

    Show default icons instead of anonymous avatar for notifications without authors.

--- bbs/src/pages/Messages/Notifications/NotificationItem.tsx
+++ bbs/src/pages/Messages/Notifications/NotificationItem.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from 'react-router-dom'
 
+import { CollectionsBookmark, InfoOutlined } from '@mui/icons-material'
 import { Box, ListItem, ListItemButton, Stack, Typography } from '@mui/material'
 
 import { readNotification } from '@/apis/messages'
@@ -23,13 +24,36 @@ const NotificationItem = ({
   small?: boolean
 }) => {
   const navigate = useNavigate()
-  const avatar = <Avatar uid={item.author_id} sx={{ mr: 1 }} />
+  const avatar = item.author_id ? (
+    <Avatar uid={item.author_id} sx={{ mr: 1 }} />
+  ) : (
+    <Stack
+      width={40}
+      height={40}
+      justifyContent="center"
+      alignItems="center"
+      mr={1}
+    >
+      {item.kind == 'thread_collect' ? (
+        <CollectionsBookmark
+          htmlColor="#aaaaaa"
+          sx={{ width: 28, height: 28 }}
+        />
+      ) : (
+        <InfoOutlined htmlColor="#aaaaaa" sx={{ width: 28, height: 28 }} />
+      )}
+    </Stack>
+  )
   const body = (
     <Stack direction="row">
       {summary ? (
         avatar
       ) : (
-        <Link to={pages.user({ uid: item.author_id })}>{avatar}</Link>
+        <Link
+          to={item.author_id ? pages.user({ uid: item.author_id }) : undefined}
+        >
+          {avatar}
+        </Link>
       )}
       <Box sx={{ wordBreak: 'break-word', overflowWrap: 'anywhere' }}>
         {!summary && (
