80e64d23a05840eb646bad24d3e17c4544e8bb9d
方觉 (Fang Jue) <fangjue23303@gmail.com>
2024-10-28 08:12:28 +0800

    Add more dialog for announcement.

--- bbs/src/components/Announcement/index.tsx
+++ bbs/src/components/Announcement/index.tsx
@@ -2,18 +2,28 @@ import 'swiper/css'
 import 'swiper/css/autoplay'
 import 'swiper/css/pagination'
 import { Autoplay, Pagination } from 'swiper/modules'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperRef, SwiperSlide } from 'swiper/react'
 
-import { useEffect } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { useMatches } from 'react-router-dom'
 
-import { Campaign } from '@mui/icons-material'
+import {
+  ArrowBackIosNew,
+  ArrowForwardIos,
+  Campaign,
+  More,
+} from '@mui/icons-material'
 import {
   Box,
+  Button,
+  IconButton,
+  List,
+  ListItem,
   Skeleton,
   Stack,
   SxProps,
   Typography,
+  useMediaQuery,
   useTheme,
 } from '@mui/material'
 
@@ -21,11 +31,13 @@ import { getIndexData } from '@/apis/common'
 import { Announcement as AnnouncementItem } from '@/common/interfaces/response'
 import { useAppState } from '@/states'
 
+import GeneralDialog from '../GeneralDialog'
 import Link from '../Link'
 
 export const AnnouncementBox = ({
   children,
   sx,
+  onMoreClick,
   ...other
 }: {
   children?: React.ReactNode
@@ -33,6 +45,7 @@ export const AnnouncementBox = ({
   mb?: number
   m?: number
   width?: string
+  onMoreClick?: () => void
 }) => {
   const theme = useTheme()
   return (
@@ -48,9 +61,12 @@ export const AnnouncementBox = ({
         justifyContent="center"
         alignItems="center"
         width={48}
+        flexGrow={0}
+        flexShrink={0}
         sx={{
           backgroundColor: theme.palette.primary.main,
         }}
+        onClick={onMoreClick}
       >
         <Campaign fontSize="large" sx={{ color: theme.palette.grey[300] }} />
       </Stack>
@@ -62,9 +78,11 @@ export const AnnouncementBox = ({
 export const AnnouncementBody = ({
   item,
   sx,
+  fullText,
 }: {
   item: AnnouncementItem
   sx?: SxProps
+  fullText?: boolean
 }) => {
   const theme = useTheme()
   const highlightColor = !item.highlight_color
@@ -91,7 +109,10 @@ export const AnnouncementBody = ({
             {item.title}
           </Typography>
         </Link>
-        <Typography className="line-clamp-1" variant="threadItemSummary">
+        <Typography
+          className={fullText ? undefined : 'line-clamp-1'}
+          variant="threadItemSummary"
+        >
           {item.summary}
           <Link to={item.href} underline="none">
             【点我查看】
@@ -102,10 +123,67 @@ export const AnnouncementBody = ({
   )
 }
 
+const MoreDialog = ({
+  onClose,
+  announcement,
+}: {
+  onClose: () => void
+  announcement?: AnnouncementItem[]
+}) => {
+  const theme = useTheme()
+  const narrowView = useMediaQuery('(max-width: 640px)')
+
+  return (
+    <GeneralDialog
+      titleText="本站公告"
+      open
+      onClose={onClose}
+      actions={[
+        {
+          type: 'ok',
+          onClick: async () => {
+            onClose()
+          },
+        },
+      ]}
+      maxWidth="md"
+      fullWidth
+      PaperProps={
+        narrowView
+          ? { sx: { m: 0, width: '100%', maxHeight: '100%' } }
+          : undefined
+      }
+    >
+      <List disablePadding>
+        {announcement?.map((item, index) => (
+          <ListItem key={index} sx={{ p: 0, my: 1 }} onClick={onClose}>
+            <AnnouncementBody
+              item={item}
+              sx={{
+                width: '100%',
+                ...(theme.palette.mode == 'light'
+                  ? {
+                      backgroundColor:
+                        theme.palette.background.paperHighlighted,
+                    }
+                  : undefined),
+              }}
+              fullText
+            />
+          </ListItem>
+        ))}
+      </List>
+    </GeneralDialog>
+  )
+}
+
 const Announcement = ({ inSwiper }: { inSwiper?: boolean }) => {
   const theme = useTheme()
   const { state, dispatch } = useAppState()
+  const [moreOpen, setMoreOpen] = useState(false)
   const matches = useMatches()
+  const swiperRef = useRef<SwiperRef>(null)
+  const narrowView = useMediaQuery('(max-width: 640px)')
   useEffect(() => {
     if (
       matches.length &&
@@ -138,6 +216,7 @@ const Announcement = ({ inSwiper }: { inSwiper?: boolean }) => {
           '--swiper-pagination-bullet-inactive-color': '#ccc',
           '--swiper-pagination-bullet-inactive-opacity': 1,
         }}
+        onMoreClick={() => setMoreOpen(true)}
       >
         <Swiper
           modules={[Autoplay, Pagination]}
@@ -147,6 +226,7 @@ const Announcement = ({ inSwiper }: { inSwiper?: boolean }) => {
           loop={state.announcement.length > 1}
           nested={inSwiper}
           css={{ flexGrow: 1, flexShrink: 1 }}
+          ref={swiperRef}
         >
           {state.announcement.map((item, index) => (
             <SwiperSlide key={index}>
@@ -154,6 +234,35 @@ const Announcement = ({ inSwiper }: { inSwiper?: boolean }) => {
             </SwiperSlide>
           ))}
         </Swiper>
+        {!narrowView && state.announcement.length > 1 && (
+          <Stack
+            sx={{ backgroundColor: theme.palette.background.paperHighlighted }}
+          >
+            <Stack direction="row">
+              <IconButton
+                color="info"
+                onClick={() => swiperRef.current?.swiper?.slidePrev()}
+              >
+                <ArrowBackIosNew />
+              </IconButton>
+              <IconButton
+                color="info"
+                onClick={() => swiperRef.current?.swiper?.slideNext()}
+              >
+                <ArrowForwardIos />
+              </IconButton>
+            </Stack>
+            <Button onClick={() => setMoreOpen(true)}>
+              <More />
+            </Button>
+          </Stack>
+        )}
+        {moreOpen && (
+          <MoreDialog
+            announcement={state.announcement}
+            onClose={() => setMoreOpen(false)}
+          />
+        )}
       </AnnouncementBox>
     )
   }
