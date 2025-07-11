57a4f25f8b99f013ad2994a03d3b53b3e1bec10d
方觉 (Fang Jue) <fangjue23303@gmail.com>
2025-07-11 15:10:29 +0800

    Set font size for input to 16px to avoid automatic zooming in on iOS.

--- bbs/src/components/ThemeProvider/Theme/base.ts
+++ bbs/src/components/ThemeProvider/Theme/base.ts
@@ -54,6 +54,13 @@ const baseComponent = {
       },
     ],
   },
+  MuiInputBase: {
+    styleOverrides: {
+      input: {
+        fontSize: 16,
+      },
+    },
+  },
 }
 
 export const getComponentsByMode = (mode: 'dark' | 'light') => ({
