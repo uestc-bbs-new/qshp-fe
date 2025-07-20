9325f6dce722aa034373a358dcd1fb04b7fea107
方觉 (Fang Jue) <fangjue23303@gmail.com>
2025-07-20 21:25:41 +0800

    Update isVpnProxy check.

--- bbs/src/utils/siteRoot.ts
+++ bbs/src/utils/siteRoot.ts
@@ -3,5 +3,4 @@ const prefix = import.meta.env.DEV ? `https://${siteDomain}` : ''
 export default prefix
 
 export const isVpnProxy = // @ts-expect-error preserve code as is
-  window[['L'.toLowerCase(), 'ocation'].join('')].hostname ==
-  `bbs-uestc-edu-cn-s.vpn.uestc.edu.cn`
+  !!(window.vpnGlobal && window.__vpn_hostname_data == 'webvpn.uestc.edu.cn')
