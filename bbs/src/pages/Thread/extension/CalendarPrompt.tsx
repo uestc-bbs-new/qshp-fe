3e685b0b080983c414bce73a77a233110e372ae4
方觉 (Fang Jue) <fangjue23303@gmail.com>
2025-09-08 06:51:16 +0800

    Calendar: show week number in a separate line on mobile.

--- bbs/src/pages/Thread/extension/CalendarPrompt.tsx
+++ bbs/src/pages/Thread/extension/CalendarPrompt.tsx
@@ -1,14 +1,18 @@
+import { useMediaQuery } from '@mui/material'
+
 import { getTermWeek } from '@/utils/calendar'
 
 const CalendarPrompt = () => {
   const week = getTermWeek()
+  const mobile = useMediaQuery('(max-width: 560px)')
   if (week) {
     const today = new Date()
     return (
       <p css={{ fontSize: 20, color: '#006eff', textAlign: 'center' }}>
         今天是 {today.getFullYear()} 年 {today.getMonth() + 1} 月{' '}
-        {today.getDate()} 日星期{'日一二三四五六'[today.getDay()]}，本周为第{' '}
-        {week} 周。
+        {today.getDate()} 日星期{'日一二三四五六'[today.getDay()]}，
+        {mobile && <br />}
+        本周为第 {week} 周。
       </p>
     )
   }
